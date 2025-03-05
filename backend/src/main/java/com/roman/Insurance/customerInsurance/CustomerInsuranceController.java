8f3a41732a8588c1c159f434cbe217684e700932
Roman Trnka <trnka.roman13@gmail.com>
2025-03-05 21:10:48 +0100

    added http status for creating of insurance

--- backend/src/main/java/com/roman/Insurance/customerInsurance/CustomerInsuranceController.java
+++ backend/src/main/java/com/roman/Insurance/customerInsurance/CustomerInsuranceController.java
@@ -2,6 +2,7 @@ package com.roman.Insurance.customerInsurance;
 
 import com.roman.Insurance.customerInsurance.request.CustomerTravelInsuranceRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,7 @@ public class CustomerInsuranceController {
 
         customerInsuranceService.createTravelInsurance(customerTravelInsuranceRequest);
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
 }
